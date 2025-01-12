d12f89b391cdff6233dc5673fc0d203be8bf00f9
Lucas-Beijato <lucasbeijato0@gmail.com>
2025-01-12 13:34:32 -0400

    add cloudflare Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
   output: "server",
   integrations: [tailwind()],
 });
